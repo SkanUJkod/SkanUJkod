bfe4b88919c358d1ea068d83149642e4565c75bd
Frang84 <skorniak.skorniak@wp.pl>
2025-04-10 23:47:08 +0200

    grammar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
 fn main() {
-    let greeting = "Hello, World!".to_string();
-    println!("{greeting}");
+    parse_file();
+}
+
+
+fn parse_file() {
+    let source = "package main";
+    let mut fs = go_parser::FileSet::new();
+    let o = &mut go_parser::AstObjects::new();
+    let el = &mut go_parser::ErrorList::new();
+    let (p, ast) = go_parser::parse_file(o, &mut fs, el, "./main.go", source, false);
+    // print!("{}", p.get_errors());
+    println!("{:?}", p.get_errors());
+    println!("{:?}", o.idents.vec());
+
+
+    for ao in o.fdecls.vec() {
+        println!("{:?}", ao);
+    }
 }
