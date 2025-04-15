3ea9052b074c9d45338bc5d9407fc33d6b94d082
Jakub Magiera <jakubmagiera.it@gmail.com>
2025-04-15 20:40:15 +0200

    add run_selected_metrics function for development use

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
-fn main() {
-    let greeting = "Hello, World!".to_string();
-    println!("{greeting}");
+mod metrics;
+mod repo;
+mod runner;
+
+use std::collections::HashMap;
+use repo::RepoWrapper;
+use runner::run_selected_metrics;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let repo = RepoWrapper::new(".");
+    let selected = vec!["contributors_in_timeframe"];
+
+    let mut all_params: HashMap<String, HashMap<String, String>> = HashMap::new();
+
+    let end_date = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() as i64;
+    let start_date = end_date - (30 * 24 * 60 * 60);
+
+    all_params.insert(
+        "contributors_in_timeframe".to_string(),
+        HashMap::from([
+            ("start_date".to_string(), start_date.to_string()),
+            ("end_date".to_string(), end_date.to_string())
+        ]),
+    );
+
+    run_selected_metrics(&repo, selected, &all_params);
+
+    Ok(())
 }
