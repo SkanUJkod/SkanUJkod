39108d462983f97ec2c830d9d9d778d7beac1584
Jakub Magiera <jakubmagiera.it@gmail.com>
2025-04-22 16:49:39 +0200

    add result type and utils

---
+++ src/metrics/utils.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+use super::{metrics_trait::Metric, result_type::MetricResultType};
+
+pub fn parse_param(params: &HashMap<String, String>, key: &str, default: i64) -> i64 {
+    params.get(key).and_then(|s| s.parse::<i64>().ok()).unwrap_or(default)
+}
+
+pub fn print_results(metrics: &Vec<&dyn Metric>, all_results: &HashMap<&str, MetricResultType>) {
+    for metric in metrics {
+        let name = metric.name();
+        let result = all_results.get(name).unwrap();
+        println!("--- {} ---", name);
+        match result {
+            MetricResultType::Map(map) => {
+                for (key, value) in map {
+                    println!("{}: {}", key, value);
+                }
+            }
+            MetricResultType::Set(set) => {
+                for value in set {
+                    println!("{}", value);
+                }
+            }
+        }
+        println!("--- --- ---\n");
+    }
+}
