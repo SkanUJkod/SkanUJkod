5d7ca6c07466a61f9e3e93041562a8bc7514d679
Jakub Magiera <jakubmagiera.it@gmail.com>
2025-04-24 21:56:25 +0200

    add PercentageOfTotalCommits and FirstLastCommit

--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -3,12 +3,16 @@ pub mod metrics_trait;
 pub mod result_type;
 pub mod utils;
 
-use commits::{CommitsByAuthorInRepo, ContributorsInTimeframe};
+use commits::{
+    CommitsByAuthorInRepo, ContributorsInTimeframe, FirstLastCommit, PercentageOfTotalCommits,
+};
 use metrics_trait::Metric;
 
 pub fn all_metrics() -> Vec<Box<dyn Metric>> {
     vec![
         Box::new(CommitsByAuthorInRepo),
         Box::new(ContributorsInTimeframe),
+        Box::new(PercentageOfTotalCommits),
+        Box::new(FirstLastCommit),
     ]
 }
