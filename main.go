6f1279764dd80c6360a213a95369b56e3d4e5397
Frang84 <skorniak.skorniak@wp.pl>
2025-04-25 10:39:45 +0200

    refactored code didn't recognize type of check. For fun_name check name_viaolation function was matching every type (type, var ,...). Remedy for thaht were discriminant function used in match insted of matches! macro

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+type type1 struct {
+	ID   int
+	Name string
+}
+type type2 struct {
+	ID   int
+	Name string
+}
+type type3 struct {
+	ID   int
+	Name string
+}
+
+func foo1() {
+	print("foo1")
+}
+
+func foo2() {
+	print("foo2")
+}
+func foo3() {
+	print("foo3")
+}
+func foo4() {
+	print("foo4")
+}
+
+func main() {
+	// Example usage
+
+}
