8b701353b1a1bc518cb30881fd5894c96191af08
Jakub Magiera <jakubmagiera.it@gmail.com>
2025-05-12 20:34:31 +0200

    simplify metric results printing

--- src/metrics/result_type.rs
+++ src/metrics/result_type.rs
@@ -1,6 +1,7 @@
 use chrono::{DateTime, Utc};
 use std::collections::{HashMap, HashSet};
 
+#[derive(Debug)]
 pub enum MetricResultType {
     Map(HashMap<String, i32>),
     DatePair(HashMap<String, (DateTime<Utc>, DateTime<Utc>)>),
