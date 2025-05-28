bb2cec3dfd228dfdc083914e64527d6ac1af885b
Jakub Magiera <jakubmagiera.it@gmail.com>
2025-05-28 22:19:46 +0200

    add parsing string param

--- src/metrics/utils.rs
+++ src/metrics/utils.rs
@@ -1,11 +1,19 @@
 use super::{metrics_trait::Metric, result_type::MetricResultType};
 use std::collections::HashMap;
 
-pub fn parse_param(params: &HashMap<String, String>, key: &str, default: i64) -> i64 {
+pub fn parse_param_i64(params: &HashMap<String, String>, key: &str, default: i64) -> i64 {
     params
         .get(key)
         .and_then(|s| s.parse::<i64>().ok())
         .unwrap_or(default)
+}
+
+pub fn parse_param_string<'a>(
+    params: &'a HashMap<String, String>,
+    key: &str,
+    default: &'a String,
+) -> &'a String {
+    params.get(key).unwrap_or(default)
 }
 
 pub fn print_results(metrics: &Vec<&dyn Metric>, all_results: &HashMap<&str, MetricResultType>) {
